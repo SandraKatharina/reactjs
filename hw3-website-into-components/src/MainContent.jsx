2d48f994f4cdb908e5f36eb345526f5a69c65046
Sandra Katharina <joceepaul@me.com>
2023-05-26 18:08:58 +0200

    modify text

--- hw3-website-into-components/src/MainContent.jsx
+++ hw3-website-into-components/src/MainContent.jsx
@@ -4,11 +4,14 @@ function MainContent({ name, counter }) {
       <p>{`Hello ${name || "Visitor"}`},</p>
       <br />
 
-      {counter > 0
-        ? `You have ${
-            counter || 0
-          } unread messages. Please go to your inbox to read them.`
-        : `You have no new messages.`}
+      {counter > 0 ? (
+        <>
+          <p>{`You have ${counter || 0} unread messages.`}</p>
+          <p>Please go to your inbox to read them.</p>
+        </>
+      ) : (
+        <p>You have no new messages.</p>
+      )}
     </article>
   );
 }
